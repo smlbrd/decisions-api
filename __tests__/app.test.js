0763cc690ce8c1b014c45cc7b1da527025de2fb4
absydavsy <abbydavis@outlook.com>
2025-01-14 11:02:28 +0000

    post list and post tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
-const request = require('supertest');
-const mongoose = require('mongoose');
-const app = require('../app');
-const seed = require('../database/seeds/seed');
-const usersData = require('../database/test-data/test-users');
-const groupsData = require('../database/test-data/test-groups');
-const listsData = require('../database/test-data/test-lists');
-const optionsData = require('../database/test-data/test-options');
+require("dotenv").config();
+const request = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../app");
+const seed = require("../database/seeds/seed");
+const usersData = require("../database/test-data/test-users");
+const groupsData = require("../database/test-data/test-groups");
+const listsData = require("../database/test-data/test-lists");
+const optionsData = require("../database/test-data/test-options");
 
 const uri = process.env.DATABASE_URI;
 
@@ -22,27 +22,27 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-describe('GET /', () => {
-  test('200: API health check', async () => {
-    const response = await request(app.callback()).get('/');
+describe("GET /", () => {
+  test("200: API health check", async () => {
+    const response = await request(app.callback()).get("/");
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Server online!');
+    expect(response.text).toBe("Server online!");
   });
 });
 
-describe('GET /users/:userId', () => {
-  test('200: responds with user for corresponding ID', async () => {
-    const testId = '6784d64b844f23ac9810cf21';
+describe("GET /users/:userId", () => {
+  test("200: responds with user for corresponding ID", async () => {
+    const testId = "6784d64b844f23ac9810cf21";
 
     const response = await request(app.callback()).get(`/users/${testId}`);
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(
       expect.objectContaining({
-        _id: '6784d64b844f23ac9810cf21',
-        username: 'sparkle_unicorn',
-        name: 'Sparkle Unicorn',
-        email: 'sparkle@testmail.com',
+        _id: "6784d64b844f23ac9810cf21",
+        username: "sparkle_unicorn",
+        name: "Sparkle Unicorn",
+        email: "sparkle@testmail.com",
         createdAt: expect.any(String),
         __v: 0,
       })
@@ -50,24 +50,88 @@ describe('GET /users/:userId', () => {
   });
 });
 
-describe('GET /groups/:groupId', () => {
-  test('200: responds with group for corresponding ID', async () => {
-    const groupId = '6784d715844f23ac9810cf28';
+describe("GET /groups/:groupId", () => {
+  test("200: responds with group for corresponding ID", async () => {
+    const groupId = "6784d715844f23ac9810cf28";
 
     const response = await request(app.callback()).get(`/groups/${groupId}`);
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(
       expect.objectContaining({
-        _id: '6784d715844f23ac9810cf28',
-        name: 'test_group',
-        description: 'A group for all things testing',
+        _id: "6784d715844f23ac9810cf28",
+        name: "test_group",
+        description: "A group for all things testing",
         members: [
-          '6784d64b844f23ac9810cf21',
-          '6784d64b844f23ac9810cf22',
-          '6784d64b844f23ac9810cf23',
+          "6784d64b844f23ac9810cf21",
+          "6784d64b844f23ac9810cf22",
+          "6784d64b844f23ac9810cf23",
         ],
         createdAt: expect.any(String),
+        __v: 0,
+      })
+    );
+  });
+});
+
+describe("POST /groups", () => {
+  test("201: responds with newly posted group", async () => {
+    const testGroup = {
+      _id: "6784d715844f23ac90876222",
+      name: "good_gardeners",
+      description: "A group for good gardening",
+      members: [
+        { _id: "6784d64b844f23ac9810cf24" },
+        { _id: "6784d64b844f23ac9810cf25" },
+        { _id: "6784d64b844f23ac9810cf26" },
+      ],
+    };
+    const response = await request(app.callback())
+      .post("/groups")
+      .send(testGroup);
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        _id: "6784d715844f23ac90876222",
+        name: "good_gardeners",
+        description: "A group for good gardening",
+        members: [
+          "6784d64b844f23ac9810cf24",
+          "6784d64b844f23ac9810cf25",
+          "6784d64b844f23ac9810cf26",
+        ],
+        createdAt: expect.any(String),
+        __v: 0,
+      })
+    );
+  });
+});
+
+describe("POST /lists", () => {
+  test("201: responds with newly posted list", async () => {
+    const testList = {
+      _id: "6784d7a5844f23ac9810ca45",
+      title: "Gardening",
+      description: "A list of the best new gardening tools",
+      options: ["6784d7b5844f23ac9810cf34", "6784d7b5844f23ac9810cf35"],
+      owner: "6784d64b844f23ac9810cf24",
+      members: ["6784d64b844f23ac9810cf25", "6784d64b844f23ac9810cf26"],
+    };
+
+    const response = await request(app.callback())
+      .post("/lists")
+      .send(testList);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        title: "Gardening",
+        description: "A list of the best new gardening tools",
+        options: ["6784d7b5844f23ac9810cf34", "6784d7b5844f23ac9810cf35"],
+        owner: "6784d64b844f23ac9810cf24",
+        members: ["6784d64b844f23ac9810cf25", "6784d64b844f23ac9810cf26"],
+        createdAt: expect.any(String),
+        _id: "6784d7a5844f23ac9810ca45",
         __v: 0,
       })
     );
--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const connectDB = require('./database/database');
-const koa = require('koa');
+const connectDB = require("./database/database");
+const koa = require("koa");
 const app = new koa();
-const Router = require('koa-router');
-const userController = require('./controllers/users.controller');
-const listController = require('./controllers/lists.controller');
-const groupController = require('./controllers/groups.controller');
+const Router = require("koa-router");
+const userController = require("./controllers/users.controller");
+const listController = require("./controllers/lists.controller");
+const groupController = require("./controllers/groups.controller");
 const route = new Router();
-const bodyParser = require('koa-bodyparser');
+const bodyParser = require("koa-bodyparser");
 
 connectDB();
 
 app.use(bodyParser());
 app.use(route.routes());
 
-route.get('/', (ctx) => {
+route.get("/", (ctx) => {
   ctx.status = 200;
-  ctx.message = 'OK';
-  ctx.body = 'Server online!';
+  ctx.message = "OK";
+  ctx.body = "Server online!";
 });
-route.get('/users', userController.getUsers);
-route.get('/users/:userId', userController.getUserById);
-route.get('/lists/:listId', listController.getListByListId);
-route.get('/groups/:groupId', groupController.getGroupById);
-route.post('/groups', groupController.postGroup);
+route.get("/users", userController.getUsers);
+route.get("/users/:userId", userController.getUserById);
+route.get("/lists/:listId", listController.getListByListId);
+route.get("/groups/:groupId", groupController.getGroupById);
+route.post("/groups", groupController.postGroup);
+route.post("/lists", listController.postList);
 
 module.exports = app;
