f801636f7242a9f09b0d01c19bf58da98aa7e2b8
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-01-14 14:41:03 +0000

    error handling for GET /lists/:listId

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
-const request = require("supertest");
-const mongoose = require("mongoose");
-const app = require("../app");
-const seed = require("../database/seeds/seed");
-const usersData = require("../database/test-data/test-users");
-const groupsData = require("../database/test-data/test-groups");
-const listsData = require("../database/test-data/test-lists");
-const optionsData = require("../database/test-data/test-options");
+require('dotenv').config();
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../app');
+const seed = require('../database/seeds/seed');
+const usersData = require('../database/test-data/test-users');
+const groupsData = require('../database/test-data/test-groups');
+const listsData = require('../database/test-data/test-lists');
+const optionsData = require('../database/test-data/test-options');
 
 const uri = process.env.DATABASE_URI;
 
@@ -22,29 +22,27 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-describe("GET /", () => {
-  test("200: API health check", async () => {
-    const response = await request(app.callback()).get("/");
+describe('GET /', () => {
+  test('200: API health check', async () => {
+    const response = await request(app.callback()).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Server online!");
+    expect(response.text).toBe('Server online!');
   });
 });
-
 
 describe('GET /users/:userId', () => {
   test('200: responds with user for corresponding user ID', async () => {
     const testId = '6784d64b844f23ac9810cf21';
-
 
     const response = await request(app.callback()).get(`/users/${testId}`);
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(
       expect.objectContaining({
-        _id: "6784d64b844f23ac9810cf21",
-        username: "sparkle_unicorn",
-        name: "Sparkle Unicorn",
-        email: "sparkle@testmail.com",
+        _id: '6784d64b844f23ac9810cf21',
+        username: 'sparkle_unicorn',
+        name: 'Sparkle Unicorn',
+        email: 'sparkle@testmail.com',
         createdAt: expect.any(String),
         __v: 0,
       })
@@ -55,28 +53,27 @@ describe('GET /users/:userId', () => {
 
     const response = await request(app.callback()).get(`/users/${invalidId}`);
 
-    console.log(response);
     expect(response.status).toBe(404);
     expect(response.body.error).toBe('No results!');
   });
 });
 
-describe("GET /groups/:groupId", () => {
-  test("200: responds with group for corresponding ID", async () => {
-    const groupId = "6784d715844f23ac9810cf28";
+describe('GET /groups/:groupId', () => {
+  test('200: responds with group for corresponding ID', async () => {
+    const groupId = '6784d715844f23ac9810cf28';
 
     const response = await request(app.callback()).get(`/groups/${groupId}`);
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(
       expect.objectContaining({
-        _id: "6784d715844f23ac9810cf28",
-        name: "test_group",
-        description: "A group for all things testing",
+        _id: '6784d715844f23ac9810cf28',
+        name: 'test_group',
+        description: 'A group for all things testing',
         members: [
-          "6784d64b844f23ac9810cf21",
-          "6784d64b844f23ac9810cf22",
-          "6784d64b844f23ac9810cf23",
+          '6784d64b844f23ac9810cf21',
+          '6784d64b844f23ac9810cf22',
+          '6784d64b844f23ac9810cf23',
         ],
         createdAt: expect.any(String),
         __v: 0,
@@ -85,31 +82,31 @@ describe("GET /groups/:groupId", () => {
   });
 });
 
-describe("POST /groups", () => {
-  test("201: responds with newly posted group", async () => {
+describe('POST /groups', () => {
+  test('201: responds with newly posted group', async () => {
     const testGroup = {
-      _id: "6784d715844f23ac90876222",
-      name: "good_gardeners",
-      description: "A group for good gardening",
+      _id: '6784d715844f23ac90876222',
+      name: 'good_gardeners',
+      description: 'A group for good gardening',
       members: [
-        { _id: "6784d64b844f23ac9810cf24" },
-        { _id: "6784d64b844f23ac9810cf25" },
-        { _id: "6784d64b844f23ac9810cf26" },
+        { _id: '6784d64b844f23ac9810cf24' },
+        { _id: '6784d64b844f23ac9810cf25' },
+        { _id: '6784d64b844f23ac9810cf26' },
       ],
     };
     const response = await request(app.callback())
-      .post("/groups")
+      .post('/groups')
       .send(testGroup);
     expect(response.status).toBe(201);
     expect(response.body).toEqual(
       expect.objectContaining({
-        _id: "6784d715844f23ac90876222",
-        name: "good_gardeners",
-        description: "A group for good gardening",
+        _id: '6784d715844f23ac90876222',
+        name: 'good_gardeners',
+        description: 'A group for good gardening',
         members: [
-          "6784d64b844f23ac9810cf24",
-          "6784d64b844f23ac9810cf25",
-          "6784d64b844f23ac9810cf26",
+          '6784d64b844f23ac9810cf24',
+          '6784d64b844f23ac9810cf25',
+          '6784d64b844f23ac9810cf26',
         ],
         createdAt: expect.any(String),
         __v: 0,
@@ -118,43 +115,74 @@ describe("POST /groups", () => {
   });
 });
 
-describe("POST /lists", () => {
-  test("201: responds with newly posted list", async () => {
+describe('GET /lists/:listId', () => {
+  test('200: responds with a list for corresponding list ID', async () => {
+    const listId = '6784d7a5844f23ac9810cf30';
+
+    await request(app.callback())
+      .get(`/lists/${listId}`)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body._id).toBe('6784d7a5844f23ac9810cf30');
+        expect(body.title).toBe('Weekly Standup');
+        expect(body.description).toBe(
+          'A list for organizing weekly standup meetings'
+        );
+        expect(body.options.length).toBe(2);
+        expect(body.options[0]).toBe('6784d7b5844f23ac9810cf31');
+        expect(body.options[1]).toBe('6784d7b5844f23ac9810cf32');
+        expect(body.owner).toBe('6784d64b844f23ac9810cf21');
+      });
+  });
+  test('404: responds with error if cannot match list ID', async () => {
+    const invalidId = '00000a00000b00000c00000d';
+
+    await request(app.callback())
+      .get(`/lists/${invalidId}`)
+      .expect(404)
+      .then(({ body: { error } }) => {
+        expect(error).toBe('No results!');
+      });
+  });
+});
+
+describe('POST /lists', () => {
+  test('201: responds with newly posted list', async () => {
     const testList = {
-      _id: "6784d7a5844f23ac9810ca45",
-      title: "Gardening",
-      description: "A list of the best new gardening tools",
-      options: ["6784d7b5844f23ac9810cf34", "6784d7b5844f23ac9810cf35"],
-      owner: "6784d64b844f23ac9810cf24",
-      members: ["6784d64b844f23ac9810cf25", "6784d64b844f23ac9810cf26"],
+      _id: '6784d7a5844f23ac9810ca45',
+      title: 'Gardening',
+      description: 'A list of the best new gardening tools',
+      options: ['6784d7b5844f23ac9810cf34', '6784d7b5844f23ac9810cf35'],
+      owner: '6784d64b844f23ac9810cf24',
+      members: ['6784d64b844f23ac9810cf25', '6784d64b844f23ac9810cf26'],
     };
 
     const response = await request(app.callback())
-      .post("/lists")
+      .post('/lists')
       .send(testList);
 
     expect(response.status).toBe(201);
     expect(response.body).toEqual(
       expect.objectContaining({
-        title: "Gardening",
-        description: "A list of the best new gardening tools",
-        options: ["6784d7b5844f23ac9810cf34", "6784d7b5844f23ac9810cf35"],
-        owner: "6784d64b844f23ac9810cf24",
-        members: ["6784d64b844f23ac9810cf25", "6784d64b844f23ac9810cf26"],
+        title: 'Gardening',
+        description: 'A list of the best new gardening tools',
+        options: ['6784d7b5844f23ac9810cf34', '6784d7b5844f23ac9810cf35'],
+        owner: '6784d64b844f23ac9810cf24',
+        members: ['6784d64b844f23ac9810cf25', '6784d64b844f23ac9810cf26'],
         createdAt: expect.any(String),
-        _id: "6784d7a5844f23ac9810ca45",
+        _id: '6784d7a5844f23ac9810ca45',
         __v: 0,
       })
     );
   });
 });
 
-describe("Error handling middleware", () => {
-  test("404: responds with an error message for invalid route", async () => {
+describe('Error handling middleware', () => {
+  test('404: responds with an error message for invalid route', async () => {
     const response = await request(app.callback()).get(
-      "/non-existent-endpoint"
+      '/non-existent-endpoint'
     );
     expect(response.status).toBe(404);
-    expect(response.text).toBe("Not Found");
+    expect(response.text).toBe('Not Found');
   });
 });
