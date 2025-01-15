77532e6d7942bb1ede314a20a14fa9b9c94b53bc
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-01-15 08:32:55 +0000

    schema refactor

--- models/groups.model.js
+++ models/groups.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const groupSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
+  owner: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   createdAt: { type: Date, immutable: true, default: Date.now() },
 });
