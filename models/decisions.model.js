f351379265175f79f263975c2e51377033fa47c6
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-01-16 14:08:57 +0000

    updated decisions schema

--- models/decisions.model.js
+++ models/decisions.model.js
@@ -1,29 +1,43 @@
 const mongoose = require('mongoose');
 
-const decisionSchema = new mongoose.Schema({
-  list: { type: mongoose.Schema.Types.ObjectId, ref: 'List', required: true },
-  group: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', required: true },
-  votes: [
-    {
-      user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-      },
-      option: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Option',
-        required: true,
-      },
-      createdAt: { type: Date, default: Date.now() },
-    },
-  ],
-  votingStatus: {
-    type: String,
-    enum: ['open', 'closed'],
-    default: 'open',
+const decisionSchema = new mongoose.Schema(
+  {
+    list: { type: mongoose.Schema.Types.ObjectId, ref: 'List', required: true }, // reference to list used in decision
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Group',
+      required: true,
+    }, // reference to group invited to decision
+    votes: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+          required: true,
+        }, // which user voted
+        option: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Option',
+          required: true,
+        },
+      }, // what that user voted for
+      { timestamps: true }, // timestamps for vote interactions
+    ],
+    votingStatus: {
+      type: String,
+      enum: ['not started', 'in progress', 'completed'],
+      default: 'not started',
+    }, // 3 permitted states for a decision
+    decisionsProcess_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'DecisionsProcess',
+      required: true,
+    }, // reference to which process was used in a decision
+    saveData: { type: Object, default: {} }, // flexible field for capturing data needed for processes
+    completedAt: { type: Date }, // date the vote was closed / winner was decided
+    outcome: { type: mongoose.Schema.Types.ObjectId, ref: Options }, // the winner of the vote
   },
-  createdAt: { type: Date, immutable: true, default: Date.now() },
-});
+  { timestamps: true } // timestamps for decision
+);
 
 module.exports = mongoose.model('Decision', decisionSchema);
