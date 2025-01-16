823700fbac905725fa850ed3c35c86589cb8523d
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-01-16 15:07:52 +0000

    unique validation added to users.model for username and email

--- models/users.model.js
+++ models/users.model.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   bio: { type: String },
   avatarImg: { type: String },
   savedLists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }],
