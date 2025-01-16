e8506593336162c997bf8b7594b38f902b0528ab
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-01-16 13:58:44 +0000

    removing comments

--- models/decisions_processes.model.js
+++ models/decisions_processes.model.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 
 const restrictionsSchema = new mongoose.Schema(
   {
-    minGroupSize: { type: Number, required: true }, // minimum number of participants
-    maxGroupSize: { type: Number }, // optional maximum
+    minGroupSize: { type: Number, required: true },
+    maxGroupSize: { type: Number },
   },
-  { _id: false } // nested restrictions won't need/have a unique id
+  { _id: false }
 );
 
 const decisionsProcessSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true }, // e.g. "this or that", "single elimination"
-    description: { type: String, required: true }, // e.g. "choose between two options, winner stays on!"
+    name: { type: String, required: true },
+    description: { type: String, required: true },
     restrictions: { type: restrictionsSchema },
   },
-  { timestamps: true } // automatically manage createdAt and updatedAt
+  { timestamps: true }
 );
 
 module.exports = mongoose.model('DecisionsProcess', decisionsProcessSchema);
