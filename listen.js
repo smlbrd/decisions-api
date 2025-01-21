b968aaea7538961d9aa8b02bd8ed9d7593ad7554
dizzlesticks <sodha.dhiran@gmail.com>
2025-01-21 22:32:13 +0000

    update listen.js and package.json for hosting on render

--- listen.js
+++ listen.js
@@ -2,6 +2,6 @@ const server = require('../decisions-api/app');
 
 const { PORT = 9090 } = process.env;
 
-server.listen(PORT, '0.0.0.0', () =>
+server.listen(PORT, () =>
   console.log(`Server up! Listening on ${PORT}!`)
 );
